3423d014a39fe184c7e3cd4532e0b2e70480b065
bohyun <84188904+250b@users.noreply.github.com>
2021-10-13 02:12:48 +0900

    Update 09_example_1.ino

--- 09_example_1.ino
+++ 09_example_1.ino
@@ -11,15 +11,16 @@
 #define _DIST_ALPHA 0.5 // EMA weight of new sample (range: 0 to 1). Setting this value to 1 effectively disables EMA filter.
 #define M 3
 #define N_MAX 30
+int n_list[M]={3,10,30};
 
 // global variables
 float timeout; // unit: us
-float dist_min, dist_max, dist_raw,first,memo; // unit: mm
+float dist_min, dist_max, dist_raw,first,memo[M]; // unit: mm
 unsigned long last_sampling_time; // unit: ms
 float scale; // used for pulse duration to distance conversion
 float dist_list[N_MAX];
-float sorted_dist[N_MAX];
-int n_list[M],m,n,n_max,i;
+float sorted_dist[M][N_MAX];
+int m,n,n_max,i;
 float median;
 
 void setup() {
@@ -38,16 +39,19 @@ void setup() {
 // initialize serial port
   Serial.begin(57600);
 
-  //n_list={3,10,30};
   m=M;
   n_max=N_MAX;
-  n=n_max;
 // initialize last sampling time
   last_sampling_time = 0;
   first = USS_measure(PIN_TRIG,PIN_ECHO);
   for (i=0; i<n-1; i++) {
     dist_list[i]= first;
-    sorted_dist[i]=first;
+  }
+  for(int j=0;i<m;j++){
+    n=n_list[j];
+    for (i=0; i<n-1; i++) {
+      sorted_dist[j][i]= first;
+    }
   }
 }
 
@@ -56,55 +60,58 @@ void loop() {
 // millis() returns the number of milliseconds since the program started. Will overflow after 50 days.
   if(millis() < last_sampling_time + INTERVAL) return;
 
-  for(i=0;i<n-1;i++){
-    if(memo==sorted_dist[i]){
-      break;
-    }
-  }
-  for(;i<n-1;i++){
-    sorted_dist[i]=sorted_dist[i+1];
-  }
-
-// get a distance reading from the USS
+  // get a distance reading from the USS
   dist_raw = USS_measure(PIN_TRIG,PIN_ECHO);
   dist_list[n_max-1]=dist_raw;
 
-  for(i=n-1;i>0;i--){
-    if(sorted_dist[i-1]<=dist_raw){
-      break;
-    }
-    sorted_dist[i]=sorted_dist[i-1];
-  }
-  sorted_dist[i]=dist_raw;
-  
-  if (n%2==0) {
-    median = (sorted_dist[n/2-1] + sorted_dist[n/2])/2;
-  }
-  else {
-    median = sorted_dist[n/2];
-  }
-  /*for(int j=0;j<M;j++){
-    n=n_list[j];
-    for (int i=n_max-2; i>n; i++) {
-      dist_list[i]=dist_list[i-1];
-    }
-  }*/
-  memo=dist_list[0];
-  for (i=0; i<n-1; i++) {
-    dist_list[i]=dist_list[i+1];
-  }
-
-
-// output the read value to the serial port
+  // output the read value to the serial port
   Serial.print("Min:0,");
   Serial.print("raw:");
   Serial.print(dist_raw);
   Serial.print(",");
-  Serial.print("median:");
-  //Serial.print(median);
-  Serial.print(map(median,0,400,100,500));
+
+  
+  for(int j=0;j<m;j++){
+    n=n_list[j];
+  for(i=0;i<n-1;i++){
+    if(memo[j]==sorted_dist[j][i]){
+      break;
+    }
+  }
+  for(;i<n-1;i++){
+    sorted_dist[j][i]=sorted_dist[j][i+1];
+  }
+
+  for(i=n-1;i>0;i--){
+    if(sorted_dist[j][i-1]<=dist_raw){
+      break;
+    }
+    sorted_dist[j][i]=sorted_dist[j][i-1];
+  }
+  sorted_dist[j][i]=dist_raw;
+  
+  if (n%2==0) {
+    median = (sorted_dist[j][n/2-1] + sorted_dist[j][n/2])/2;
+  }
+  else {
+    median = sorted_dist[j][n/2];
+  }
+  memo[j]=dist_list[n_max-n];
+  
+
+
+
+  Serial.print("median");
+  Serial.print(n);
+  Serial.print(":");
+  Serial.print(map(median,0,400,100+50*j,500+50*j));
   Serial.print(",");
-  Serial.println("Max:500");
+  }
+  
+  for (i=0; i<n-1; i++) {
+    dist_list[i]=dist_list[i+1];
+  }
+  Serial.println("Max:600");
 
 // turn on the LED if the distance is between dist_min and dist_max
   if(dist_raw < dist_min || dist_raw > dist_max) {
